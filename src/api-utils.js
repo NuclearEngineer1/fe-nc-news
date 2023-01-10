507e366752cce59bfca419ffe3e71655315dd677
NuclearEngineer1 <ljk8@hotmail.co.uk>
2023-01-10 12:28:56 +0000

    add vote on article functionality

--- src/api-utils.js
+++ src/api-utils.js
@@ -1,28 +1,41 @@
+import axios from "axios";
+
+const API = axios.create({ baseURL: "https://liams-nc-news.onrender.com/api" });
+
 export const fetchArticles = () => {
-  return fetch('https://liams-nc-news.onrender.com/api/articles').then((res) => {
-    return res.json();
-
-  }).then((res) => { 
-    return res.articles
-   })
-} 
-
-export const fetchArticleByID = (id) => { 
-    return fetch(`https://liams-nc-news.onrender.com/api/articles/${id}`)
-      .then((res) => {
-        return res.json();
-      })
-      .then((res) => {
-        return res
-      });
-}
-
-export const fetchComments = (article_id) => {
-  return fetch(`https://liams-nc-news.onrender.com/api/articles/${article_id}/comments`)
+  return fetch("https://liams-nc-news.onrender.com/api/articles")
     .then((res) => {
       return res.json();
     })
-    .then((res) => { 
-      return res.comments
+    .then((res) => {
+      return res.articles;
+    });
+};
+
+export const fetchArticleByID = (id) => {
+  return fetch(`https://liams-nc-news.onrender.com/api/articles/${id}`)
+    .then((res) => {
+      return res.json();
     })
-}
+    .then((res) => {
+      return res;
+    });
+};
+
+export const fetchComments = (article_id) => {
+  return fetch(
+    `https://liams-nc-news.onrender.com/api/articles/${article_id}/comments`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      return res.comments;
+    });
+};
+
+export const patchVotes = (article_id, voteCount) => {
+  return API.patch(`/articles/${article_id}`, {
+    inc_votes: voteCount,
+  });
+};
--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,33 +1,61 @@
-import { fetchArticleByID } from '../api-utils'
-import { useEffect, useState } from "react"
-import {useParams} from "react-router-dom"
+import { fetchArticleByID } from "../api-utils";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { patchVotes } from "../api-utils";
 
 const SingleArticle = () => {
-  const [currentArticle, setCurrentArticle] = useState({})
+  const [currentArticle, setCurrentArticle] = useState({});
+  const [isError, setIsError] = useState(false);
   const dateObj = new Date(currentArticle.created_at);
   const dateString = dateObj.toString();
-  const { article_id } = useParams()
-
+  const { article_id } = useParams();
+  const [userViewVotes, setUserViewVotes] = useState(0);
+  
 
   useEffect(() => {
     fetchArticleByID(article_id).then((article) => {
-      setCurrentArticle(article)
-    })
-  }, [article_id])
+      setCurrentArticle(article);
+    });
+  }, [article_id]);
+
+  const handleVoteClick = (increment) => {
+    setUserViewVotes(userViewVotes + increment);
+    patchVotes(currentArticle.article_id, increment).catch((err) => {
+      console.log(err);
+      setUserViewVotes(userViewVotes - increment);
+      setIsError(true);
+    });
+  };
 
 
-  return (
-    <div>
-    <h2>{currentArticle.title}</h2>
-    <h3> Author: {currentArticle.author} </h3>
-    <p>Created: {dateString}</p>
-    <p>Topic: {currentArticle.topic}</p>
-      <p>{currentArticle.body}</p>
-    <p> Votes: {currentArticle.votes} </p>
-    <p> Comments: {currentArticle.comment_count}</p>
-    </div>
-  )
+  if (isError) {
+    return (
+      <div>
+        <h2>{currentArticle.title}</h2>
+        <h3> Author: {currentArticle.author} </h3>
+        <p>Created: {dateString}</p>
+        <p>Topic: {currentArticle.topic}</p>
+        <p>{currentArticle.body}</p>
+        <p> Votes: {currentArticle.votes + userViewVotes} </p>
+        <p> Sorry there was an internal server error involving your vote</p>
+        <p> Comments: {currentArticle.comment_count}</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h2>{currentArticle.title}</h2>
+        <h3> Author: {currentArticle.author} </h3>
+        <p>Created: {dateString}</p>
+        <p>Topic: {currentArticle.topic}</p>
+        <p>{currentArticle.body}</p>
+        <p> Votes: {currentArticle.votes + userViewVotes} </p>
+        <button onClick={() => {handleVoteClick(1)}}>Upvote</button>
+        <button onClick={() => {handleVoteClick(-1)}}>Downvote</button>
+        <p> Comments: {currentArticle.comment_count}</p>
+      </div>
+    );
+  }
+};
 
-}
-
-export default SingleArticle
+export default SingleArticle;
