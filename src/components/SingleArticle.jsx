b0b5d84f3ad82002a98d36b8460245580bc0b799
NuclearEngineer1 <ljk8@hotmail.co.uk>
2023-01-10 14:05:24 +0000

    add loading message for ArticleList, SingleArticle and CommentList

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,33 +1,36 @@
-import { fetchArticleByID } from '../api-utils'
-import { useEffect, useState } from "react"
-import {useParams} from "react-router-dom"
+import { fetchArticleByID } from "../api-utils";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const SingleArticle = () => {
-  const [currentArticle, setCurrentArticle] = useState({})
+  const [currentArticle, setCurrentArticle] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
   const dateObj = new Date(currentArticle.created_at);
   const dateString = dateObj.toString();
-  const { article_id } = useParams()
-
+  const { article_id } = useParams();
 
   useEffect(() => {
     fetchArticleByID(article_id).then((article) => {
-      setCurrentArticle(article)
-    })
-  }, [article_id])
+      setCurrentArticle(article);
+      setIsLoading(false);
+    });
+  }, [article_id]);
 
+  if (isLoading) {
+    return <p>Loading...</p>;
+  } else {
+    return (
+      <div>
+        <h2>{currentArticle.title}</h2>
+        <h3> Author: {currentArticle.author} </h3>
+        <p>Created: {dateString}</p>
+        <p>Topic: {currentArticle.topic}</p>
+        <p>{currentArticle.body}</p>
+        <p> Votes: {currentArticle.votes} </p>
+        <p> Comments: {currentArticle.comment_count}</p>
+      </div>
+    );
+  }
+};
 
-  return (
-    <div>
-    <h2>{currentArticle.title}</h2>
-    <h3> Author: {currentArticle.author} </h3>
-    <p>Created: {dateString}</p>
-    <p>Topic: {currentArticle.topic}</p>
-      <p>{currentArticle.body}</p>
-    <p> Votes: {currentArticle.votes} </p>
-    <p> Comments: {currentArticle.comment_count}</p>
-    </div>
-  )
-
-}
-
-export default SingleArticle
+export default SingleArticle;
