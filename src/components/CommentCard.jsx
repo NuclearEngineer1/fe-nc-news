812e2d1ee05e71c6243a9322b4250904eee58d35
NuclearEngineer1 <ljk8@hotmail.co.uk>
2023-01-12 16:58:07 +0000

    add delete comment functionality and add dysfunctional error message

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,24 +1,36 @@
 import { deleteComment } from "../api-utils";
+import {useState} from "react"
 
 const CommentCard = (props) => {
   const dateObj = new Date(props.created_at);
   const dateString = dateObj.toString();
 
+  const [deleteCommentError, setDeleteCommentError] = useState(false);
+
   const handleDeleteComment = () => {
     if (props.author === "tickle122") {
-      console.log(props.commentData)
-      console.log(props.comment_id)
       const commentIndex = props.commentData.findIndex(
         (comment) => comment.comment_id === props.comment_id
       );
-      console.log(commentIndex)
+      const comment = props.commentData.filter(
+        (comment) => comment.comment_id === props.comment_id
+      )[0];
+      console.log(comment)
+      console.log(commentIndex);
       props.setCommentData((commentData) => {
-        return commentData.filter(comment => comment.comment_id !== props.comment_id);
+        return commentData.filter(
+          (comment) => comment.comment_id !== props.comment_id
+        );
       });
-      deleteComment(props.comment_id).catch((err) => console.log(err));
+      deleteComment(props.comment_id).catch((err) => {
+        props.setCommentData((commentData) => {
+          return { comment, ...commentData };
+        });
+        setDeleteCommentError(true);
+        console.log(err);
+      });
     }
-  }
-
+  };
 
   return (
     <li>
@@ -26,7 +38,11 @@ const CommentCard = (props) => {
       <p>Created: {dateString}</p>
       <p>{props.body}</p>
       <p> Votes: {props.votes} </p>
-      <button onClick={handleDeleteComment}>Delete Comment</button>
+      {deleteCommentError ? (
+        <p> Sorry, there was an error deleting your comment.</p>
+      ) : (
+        <button onClick={handleDeleteComment}>Delete Comment</button>
+      )}
     </li>
   );
 };
