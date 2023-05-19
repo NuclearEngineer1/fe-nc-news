e13ef0ba78d173702278da589a5921d4bbe96982
NuclearEngineer1 <ljk8@hotmail.co.uk>
2023-05-19 20:48:33 +0100

    add StyledArticleCard

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,12 +1,33 @@
 import { Link } from "react-router-dom";
-import "../CSS_files/ArticleCard.css"
+import StyledArticleCard from "./styled-components"
+import { useSearchParams } from "react-router-dom";
 
 const ArticleCard = (props) => {
   const dateObj = new Date(props.created_at);
   const dateString = dateObj.toString();
-
+  const [searchParams] = useSearchParams();
+  if (searchParams.get("region") === "scotland") {
+    return (
+      <StyledArticleCard>
+        <h2>{props.title}</h2>
+        <p>
+          <b>Author:&nbsp;</b> {props.author}{" "}
+        </p>
+        <p>
+          <b>Created:&nbsp;</b> {dateString}
+        </p>
+        <p>
+          <b>Topic:&nbsp;</b> {props.topic}
+        </p>
+        <p> <b> Comments:&nbsp; </b> {props.comment_count}</p>
+        <Link to={`article/${props.article_id}`}>
+          <button> <b> Go to article </b> </button>
+        </Link>
+      </StyledArticleCard>
+    );
+  } else {
   return (
-    <li class="ArticleCard">
+    <StyledArticleCard>
       <h2>{props.title}</h2>
       <p>
         <b>Author:&nbsp;</b> {props.author}{" "}
@@ -22,8 +43,9 @@ const ArticleCard = (props) => {
       <Link to={`article/${props.article_id}`}>
         <button> <b> Go to article </b> </button>
       </Link>
-    </li>
+    </StyledArticleCard>
   );
+  }
 };
 
 export default ArticleCard;
---
+++ src/components/styled-components.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const StyledArticleCard = styled.li`
+  border-style: solid;
+  padding: 2%;
+  margin: 2%;
+`;
+
+export default StyledArticleCard;
+
+
